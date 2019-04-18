5f67fd4fa934a37204de2e021ad3c0e092473366
Benjamin Frye <s3723499@student.rmit.edu.au>
2019-04-18 00:31:12 +0000

    Working on Coordinator model.

--- app/models/coordinator.rb
+++ app/models/coordinator.rb
@@ -1,2 +1,9 @@
 class Coordinator < ApplicationRecord
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    
+    attr_accessor :name, :email
+    validates :name, presence: true, length: {maximum: 50}
+    validates :email, presence: true, length: {maximum: 255},
+                      format: {with: VALID_EMAIL_REGEX}
+                    
 end
